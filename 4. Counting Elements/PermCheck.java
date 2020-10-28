26c3287c0a2f01b195bfe493712fcc54927451e8
Dariel Cruz Hernandez <darielnaturalist@gmail.com>
2020-10-28 13:25:21 -0400

    adding PermCheck solution

---
+++ 4. Counting Elements/PermCheck.java
@@ -0,0 +1,79 @@
+/**
+ A non-empty array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+is a permutation, but array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+the function should return 1.
+
+Given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [1..1,000,000,000].
+ */
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        long maxValue = A[0];
+        long totalSum = A[0];
+        Set duplicates = new HashSet<Integer>();
+        
+        for(int i = 1; i < A.length; i++){
+            totalSum += A[i];
+            if(A[i] > maxValue){
+                maxValue  = A[i];
+            }
+            
+            if(!duplicates.add(A[i])){
+                return 0;
+            }
+        }
+        
+        long totalValue = maxValue*(maxValue+1)/2;
+        
+        if(totalValue == totalSum){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
